e161ef4de8610a3336d8ce5e06eddd94e33e8004
Telmo Santos <telmo.santos@sensefly.com>
2017-04-13 11:41:42 +0200

    [ADD] Drone and relseller type on product

---
+++ odoo/local-src/sf_drone_info/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of SenseFly.
+{
+    'name': "SenseFly Drone Info",
+    'version': '1.0',
+    'depends': ['product'
+                ],
+    'author': "Telmo Santos",
+    'category': 'SenseFly',
+    'description': """
+    Extends product with the details of the drone adding the following information:
+    1. The type of drone
+    2. The type of reseller that will use this product.
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_drone_info.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ odoo/local-src/sf_drone_info/models/product_drone_info.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of sensefly.
+
+from odoo import fields, models
+
+RESELLER_TYPE_SELECTION = [
+    ('itrn', 'Internal'),
+    ('srv', 'Service'),
+    ('srvextd', 'Service Extended')
+]
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    drone_type_id = fields.Many2many('drone.type', 'rel_product_drone_type', string='Drone')
+    product_reseller_type = fields.Selection(RESELLER_TYPE_SELECTION, string='Reseller Type')
+
+
+class DroneType(models.Model):
+    _name = "drone.type"
+    _description = "Drone Type"
+
+    name = fields.Char(string='Name', required=True)
