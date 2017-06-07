1b9dd0de819e28c97cf7f09e40897928fc7a81ff
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-06-07 18:45:02 +0200

    Load companies

--- odoo/songs/install/pre.py
+++ odoo/songs/install/pre.py
@@ -5,44 +5,29 @@
 import os
 
 from base64 import b64encode
-from pkg_resources import resource_string
+from pkg_resources import resource_string, resource_stream
 
 import anthem
 
 from anthem.lyrics.records import create_or_update
+from anthem.lyrics.loaders import load_csv_stream
 
 from ..common import req
 
 
 @anthem.log
-def setup_company(ctx):
-    """ Setup company """
-    company = ctx.env.ref('base.main_company')
-    company.name = 'SenseFly'
+def setup_companies(ctx):
+    """ Setup companies """
+    content = resource_stream(req, 'data/install/res.company.csv')
+    load_csv_stream(ctx, 'res.company', content, delimiter=',')
 
-    # load logo on company
-    logo_content = resource_string(req, 'data/images/company_main_logo.png')
-    b64_logo = b64encode(logo_content)
-    company.logo = b64_logo
+    holding = ctx.env.ref('base.main_company')
 
-    with ctx.log(u'Configuring company'):
-        values = {
-            'name': "SenseFly",
-            'street': "Route de Genève 38",
-            'zip': "1033",
-            'city': "Cheseaux-Lausanne",
-            'country_id': ctx.env.ref('base.ch').id,
-            'phone': "+41 00 000 00 00",
-            'fax': "+41 00 000 00 00",
-            'email': "contact@sensefly.ch",
-            'website': "http://www.sensefly.ch",
-            'vat': "VAT",
-            'logo': b64_logo,
-            'currency_id': ctx.env.ref('base.CHF').id,
-        }
-        create_or_update(ctx, 'res.company',
-                         'base.main_company',
-                         values)
+    with ctx.log(u'Set main company logo'):
+        logo_content = resource_string(
+            req, 'data/images/company_main_logo.png')
+        b64_logo = b64encode(logo_content)
+        holding.logo = b64_logo
 
 
 @anthem.log
@@ -85,7 +70,7 @@ def change_config_parameters(ctx):
 @anthem.log
 def main(ctx):
     """ Main: creating demo data """
-    setup_company(ctx)
+    setup_companies(ctx)
     setup_language(ctx)
     admin_user_password(ctx)
     change_config_parameters(ctx)
