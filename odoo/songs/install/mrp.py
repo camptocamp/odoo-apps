1c6b4c6bd6d889c8b1194d7c7c9d5d7cc5983043
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-06-07 18:49:20 +0200

    Do settings per company

--- odoo/songs/install/mrp.py
+++ odoo/songs/install/mrp.py
@@ -7,13 +7,14 @@ import anthem
 @anthem.log
 def settings(ctx):
     """ Configure Manufacturing settings """
-    ctx.env['mrp.config.settings'].create(
-        {'company_id': ctx.env.ref('base.main_company').id,
-         'group_mrp_routings': 1,
-         'group_product_variant': False,
-         'manufacturing_lead': 0.0,
-         'module_mrp_byproduct': False,
-         'module_mrp_maintenance': False,
-         'module_mrp_mps': 1,
-         'module_mrp_plm': 1,
-         'module_quality_mrp': False}).execute()
+    for cp in ctx.env['res.company'].search([]):
+        ctx.env['mrp.config.settings'].create(
+            {'company_id': cp.id,
+             'group_mrp_routings': 1,
+             'group_product_variant': False,
+             'manufacturing_lead': 0.0,
+             'module_mrp_byproduct': False,
+             'module_mrp_maintenance': False,
+             'module_mrp_mps': 1,
+             'module_mrp_plm': 1,
+             'module_quality_mrp': False}).execute()
--- odoo/songs/install/purchase.py
+++ odoo/songs/install/purchase.py
@@ -7,17 +7,18 @@ import anthem
 @anthem.log
 def settings(ctx):
     """ Configure Purchases settings """
-    ctx.env['purchase.config.settings'].create(
-        {'company_currency_id': ctx.env.ref('base.USD').id,
-         'company_id': ctx.env.ref('base.main_company').id,
-         'group_costing_method': False,
-         'group_manage_vendor_price': False,
-         'group_product_variant': False,
-         'group_uom': False,
-         'group_warning_purchase': False,
-         'module_purchase_requisition': False,
-         'module_stock_dropshipping': False,
-         'po_double_validation': u'one_step',
-         'po_double_validation_amount': 5000.0,
-         'po_lead': 0.0,
-         'po_lock': u'edit'}).execute()
+    for cp in ctx.env['res.company'].search([]):
+        ctx.env['purchase.config.settings'].create(
+            {'company_currency_id': ctx.env.ref('base.USD').id,
+             'company_id': cp.id,
+             'group_costing_method': False,
+             'group_manage_vendor_price': False,
+             'group_product_variant': False,
+             'group_uom': False,
+             'group_warning_purchase': False,
+             'module_purchase_requisition': False,
+             'module_stock_dropshipping': False,
+             'po_double_validation': u'one_step',
+             'po_double_validation_amount': 5000.0,
+             'po_lead': 0.0,
+             'po_lock': u'edit'}).execute()
