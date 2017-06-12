5f19a5f43351f9b5dcf701b11e454d67d66b28d7
telmosensefly <telmo.santos@sensefly.com>
2017-06-12 17:07:14 +0200

    Country Free trade agreement field.

--- odoo/local-src/sf_country/models/res_country.py
+++ odoo/local-src/sf_country/models/res_country.py
@@ -4,6 +4,13 @@
 from odoo import fields, models
 
 
+class Country(models.Model):
+    _inherit = "res.country"
+
+    free_trade_agreement = fields.Boolean(string='Free Trade Agreement')
+    agreement_desc = fields.Text("Free Trade Agreement Details")
+
+
 class CountryGroup(models.Model):
     _inherit = "res.country.group"
 
