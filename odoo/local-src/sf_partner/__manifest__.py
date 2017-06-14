64f8658a352de4fef9cade270a5fd3690aaac229
telmosensefly <telmo.santos@sensefly.com>
2017-06-14 17:50:53 +0200

    Partner Intragroup

---
+++ odoo/local-src/sf_partner/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of SenseFly.
+{
+    "name": "senseFly Partner",
+    "summary": "Customize Partner",
+    "version": "10.0.1.0.0",
+    "category": "CRM",
+    "website": "https://sensefly.com",
+    "author": "Telmo Santos",
+    "application": False,
+    "installable": True,
+    "depends": [
+    ],
+    "data": [
+        'views/partner.xml'
+    ],
+    'images': ['static/description/icon.png'],
+}
