13c927bb3dcd8d4c653692d7e3c0a45cab39d793
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-06-14 13:43:42 +0200

    Remove duplicate of description from manifest

--- odoo/local-src/sf_stock/__manifest__.py
+++ odoo/local-src/sf_stock/__manifest__.py
@@ -7,9 +7,6 @@
     'author': 'Telmo Santos',
     'category': 'SenseFly',
     'depends': ['stock'],
-    'description': """
-This is a module for customization of stock related business objects.
-    """,
     'data': ['views/stock_location.xml'],
     'demo': [],
     'images': ['static/description/icon.png'],
