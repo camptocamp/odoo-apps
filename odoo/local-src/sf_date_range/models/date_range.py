bdab72a2ac5d79e245c2fe154ff294f3460e62f8
telmosensefly <telmo.santos@sensefly.com>
2017-08-11 09:43:29 +0200

    Date range constrain

--- odoo/local-src/sf_date_range/models/date_range.py
+++ odoo/local-src/sf_date_range/models/date_range.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of sensefly.
 
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class DateRange(models.Model):
     _inherit = "date.range"
 
-    code = fields.Char()
+    code = fields.Char(required=True)
+
+    _sql_constraints = [('uniq_code', 'unique(code)',
+                         _("The code of this range must be unique !"))]
