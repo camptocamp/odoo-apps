fa8e3914db2ed20fbffff5056fbdba851f202245
a-baghirli <a.baghirli@mobilunity.com>
2017-09-11 12:35:03 +0300

    changed field and view

--- odoo/local-src/sf_rma/models/rma.py
+++ odoo/local-src/sf_rma/models/rma.py
@@ -82,8 +82,11 @@ class RMA(models.Model):
         default=False)
     to_receive = fields.Boolean(
         default=False)
-    to_invoice = fields.Boolean(
-        default=False)
+    decision = fields.Selection(
+        [('free','Free'),
+         ('to_invoice','To invoice'),
+         ('to_offer','Commercial Gesture')]
+    )
     to_offer = fields.Boolean(
         default=False,
         string="Commercial gesture")
