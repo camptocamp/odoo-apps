f82eab70f66577a841146afcf662786e699928af
telmosensefly <telmo.santos@sensefly.com>
2017-10-18 11:50:16 +0200

    carrier_id field access control

--- odoo/local-src/sf_sale/models/sale.py
+++ odoo/local-src/sf_sale/models/sale.py
@@ -1,39 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of sensefly.
-from lxml import etree
-import json
-
 from odoo import models, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
-                        submenu=False):
-        res = super(SaleOrder, self).fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar,
-            submenu=submenu)
-        if view_type == 'form':
-            # Delivery method is editable
-            # only for group_delivery_method_manager
-            doc = etree.XML(res['arch'])
-            if self.user_has_groups("sf_stock.group_delivery_method_manager"):
-                node = doc.xpath("//field[@name='carrier_id']")[0]
-                node.set("readonly", "0")
-                modifiers = json.loads(node.get("modifiers"))
-                modifiers['readonly'] = [
-                    ('state', 'not in', ('draft', 'sent'))]
-                node.set("modifiers", json.dumps(modifiers))
-
-            res['arch'] = etree.tostring(doc)
-        return res
-
     @api.multi
     def write(self, vals):
         for sale_order in self:
-            # Onchange order lines reset delivery method
+            # Reset delivery method
             if sale_order.state in ('draft', 'sent') and 'order_line' in vals:
                 self.carrier_id = False
         return super(SaleOrder, self).write(vals)
