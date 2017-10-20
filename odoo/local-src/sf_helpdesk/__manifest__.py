860cfdff3a049b274a39882b02d8e553178667ff
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-10-20 13:28:06 +0200

    Fix license of sf_helpdesk, AGPL is not valid as it depends on a proritary module. Fixes #35

--- odoo/local-src/sf_helpdesk/__manifest__.py
+++ odoo/local-src/sf_helpdesk/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Telmo Santos (senseFly SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Sensefly helpdesk',
     'version': '1.0',
     'summary': 'Helpdesk, Ticket Support',
     'author': "senseFly, Telmo Santos",
     'website': 'https://www.sensefly.com/',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'category': 'SenseFly',
     'images': [],
     'depends': ['helpdesk'],
