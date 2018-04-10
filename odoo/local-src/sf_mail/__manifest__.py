0720ab78e8dd477c9e17451d7ef1ef5e664fdabc
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-04-10 22:02:37 +0300

    [BSSFL-504][IMP] redefine res.partner _compute_display_name to change partner name on email form

---
+++ odoo/local-src/sf_mail/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'senseFly mail',
+    'version': '1.0',
+    'category': 'Extra',
+    'summary': 'Customized mail for senseFly',
+    'website': 'https://www.sensefly.com',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
