a66d8c316bb8395f5f53b2777a18ca3f09a3f778
telmosensefly <telmo.santos@sensefly.com>
2018-04-12 15:03:13 +0200

    Assign uuid to active and none active records

--- odoo/songs/install/entity_uuid.py
+++ odoo/songs/install/entity_uuid.py
@@ -14,11 +14,15 @@ This song will assign a random UUID to partners, products and serial numbers
 @anthem.log
 def assign_uuid(ctx):
     """Assign UUID to products, partners and serial numbers"""
-    products = ctx.env['product.product'].search([])
+    products = ctx.env['product.product'].search(
+        ['|', ('active', '=', True), ('active', '=', False)]
+    )
     for product in products:
         product.uuid = uuid.uuid4()
 
-    partners = ctx.env['res.partner'].search([])
+    partners = ctx.env['res.partner'].search(
+        ['|', ('active', '=', True), ('active', '=', False)]
+    )
     for partner in partners:
         partner.uuid = uuid.uuid4()
 
