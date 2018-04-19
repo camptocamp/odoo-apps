f69380dda65e28f94b27cc484763decf4ed2145c
telmosensefly <telmo.santos@sensefly.com>
2018-04-19 10:24:05 +0200

    active_test context

--- odoo/songs/install/entity_uuid.py
+++ odoo/songs/install/entity_uuid.py
@@ -14,15 +14,13 @@ This song will assign a random UUID to partners, products and serial numbers
 @anthem.log
 def assign_uuid(ctx):
     """Assign UUID to products, partners and serial numbers"""
-    products = ctx.env['product.product'].search(
-        ['|', ('active', '=', True), ('active', '=', False)]
-    )
+    products = ctx.env['product.product'].with_context(
+        active_test=False).search([])
     for product in products:
         product.uuid = uuid.uuid4()
 
-    partners = ctx.env['res.partner'].search(
-        ['|', ('active', '=', True), ('active', '=', False)]
-    )
+    partners = ctx.env['res.partner'].with_context(
+        active_test=False).search([])
     for partner in partners:
         partner.uuid = uuid.uuid4()
 
